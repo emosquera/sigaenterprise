f0b2fa8ffdba5dcdadf6dc1edb1ecba086722983
Herbalife <Herbalife@syslifebt01>
2017-06-24 09:27:15 -0400

    Casos de Planchado

--- sigaenterprise_backend/sigaenterprise_backend-model/src/main/java/sigaenterprise/backend/treasury/model/BudgetaryImputation.java
+++ sigaenterprise_backend/sigaenterprise_backend-model/src/main/java/sigaenterprise/backend/treasury/model/BudgetaryImputation.java
@@ -5,12 +5,10 @@
  */
 package sigaenterprise.backend.treasury.model;
 
-import java.io.Serializable;
 import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import sigaenterprise.backend.auth.model.BasicAttributes;

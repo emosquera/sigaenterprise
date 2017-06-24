793047805514163c42e8c0b028c287038050de8a
Herbalife <Herbalife@syslifebt01>
2017-06-24 09:03:48 -0400

    Adicion de Get y Set - Beneficiary.java

--- sigaenterprise_backend/sigaenterprise_backend-model/src/main/java/sigaenterprise/backend/treasury/model/Beneficiary.java
+++ sigaenterprise_backend/sigaenterprise_backend-model/src/main/java/sigaenterprise/backend/treasury/model/Beneficiary.java
@@ -5,11 +5,60 @@
  */
 package sigaenterprise.backend.treasury.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import sigaenterprise.backend.auth.model.BasicAttributes;
+
 /**
  *
  * @author martinmgb
  */
-class Beneficiary {
+@Entity
+@Table(name="beneficiarys")
+public class Beneficiary extends BasicAttributes{
     private String identificationCard;
     private String name;
+    
+    @Column(name = "identificationCard")
+    public String getIdentificationCard() {
+        return identificationCard;
+    }
+
+    public void setIdentificationCard(String identificationCard) {
+        this.identificationCard = identificationCard;
+    }
+    
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (getId() != null ? getId().hashCode() : 0);
+        return hash;
+    }
+    
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Beneficiary)) {
+            return false;
+        }
+        Beneficiary other = (Beneficiary) object;
+        return !((this.getId() == null && other.getId() != null) || (this.getId() != null && !this.getId().equals(other.getId())));
+    }
+    
+
+    @Override
+    public String toString() {
+        return "sigaenterprise.backend.treasury.model.Beneficiary[ id=" + this.getId() + " ]";
+    }
 }
